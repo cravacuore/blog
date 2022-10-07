0f42e579b4f3a88edf1d3d2e19f8e8568e3b36dc
Damián Cravacuore <cravacuore.dev@gmail.com>
2022-10-06 21:51:15 -0300

    New start! Blog using svelte template

---
+++ src/lib/config.js
@@ -0,0 +1,16 @@
+export const siteTitle = 'rawBlog'
+export const siteDescription = 'rawBasic\'s blog space'
+export const siteURL = 'cravacuore.com.ar/blog/'
+export const siteLink = 'https://github.com/cravacuore/blog'
+export const siteAuthor = 'Damián Cravacuore'
+
+// Controls how many posts are shown per page on the main blog index pages
+export const postsPerPage = 10
+
+// Edit this to alter the main nav menu. (Also used by the footer and mobile nav.)
+export const navItems = [
+	{
+		title: 'About',
+		route: '/about'
+	}
+]
---
+++ src/lib/posts/heading-links-example.md
@@ -0,0 +1,8 @@
+---
+title: "Hello World!"
+date: 2015-11-25T21:14:54-03:00
+---
+
+Hola, este es el primer post de este espacio. La idea de este blog es simplemente escribir sobre temas de interés personal, ya sean al estilo de recordatorios, opiniones de cuestiones de tecnología, programación, software libre, etcétera.
+
+Seguramente este sea el primero de unos cuantos. Nos estamos leyendo.
